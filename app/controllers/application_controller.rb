dfe78f9c68aa6c6a00911f681f2f3eb57b4d7515
zhenfeng-mf <liao.zhenfeng@moneyforward.co.jp>
2026-06-18 14:29:37 +0900

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  def hello
+    render html: "hello, world!"
+  end
+
 end
