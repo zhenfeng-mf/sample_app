15dcaaea767084b337bda24ed4d75d1694a8beb9
zhenfeng-mf <liao.zhenfeng@moneyforward.co.jp>
2026-06-19 17:14:35 +0900

    Add About action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "foo/bar"
+  get "foo/baz"
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
