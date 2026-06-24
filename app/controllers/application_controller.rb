f96fe333181850fed3a361c8a549b1168ed60b58
zhenfeng-mf <liao.zhenfeng@moneyforward.co.jp>
2026-06-24 17:14:33 +0900

    Solved rails version downgrade errors. Extract same HTML structures to erb layout file.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
 
   # Changes to the importmap will invalidate the etag for HTML responses
-  stale_when_importmap_changes
+  # stale_when_importmap_changes
 
   def hello
     render html: "hello, world!"
